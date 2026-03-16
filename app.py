49add8585b93b6ebe3f7017a37185edac54d2aa6
Dhaval Pandey <dhavalpandey@Dhavals-MacBook-Air.local>
2026-03-16 15:05:59 +0000

    seeding db

--- app.py
+++ app.py
@@ -607,6 +607,9 @@ def seed_database():
                 db.session.add(p)
             db.session.commit()
 
-if __name__ == '__main__':
+with app.app_context():
+    db.create_all()
     seed_database()
+
+if __name__ == '__main__':
     app.run(debug=True, port=5000)
